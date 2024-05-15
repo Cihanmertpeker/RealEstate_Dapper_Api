35418b26d0bd9bc3b667591a1468047e077adcb5
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-05-15 15:41:00 +0300

    Popular locations added

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultBottomGridComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultBottomGridComponentPartial.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
+using RealEstate_Dapper_UI.Dtos.BottomGridDtos;
 
 namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
